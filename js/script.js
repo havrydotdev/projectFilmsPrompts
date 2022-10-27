e5a3ef5fe04ee3771382638a53152d5350df7c0e
ivan <fistashkin0123@gmail.com>
2022-10-27 18:57:33 +0300

    final

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних фильмов?'),
+        b = prompt('На сколько оцените его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    personalMovieDB.movies[a] = b;
+}
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
+
+
---
+++ projects/script3.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let num = 50;
+
+//while (num <= 55) {
+//    console.log(num);
+//    num++;
+//}
+
+//do {
+//    console.log(num);
+//    num++;
+//}
+//while (num <=55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+
+
+
+
+    console.log(i);
+}
