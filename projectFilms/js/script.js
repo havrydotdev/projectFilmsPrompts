29629ff23e24ef3a5e8b43d77571fc10188f4d48
ivan <fistashkin0123@gmail.com>
2022-10-28 13:47:06 +0300

    final(noPracticeScripts)

--- projectFilms/js/script.js
+++ projectFilms/js/script.js
@@ -3,8 +3,6 @@
 let numberOfFilms;
 
 function start() {
-    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
     }
